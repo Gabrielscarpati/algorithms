a1efef4d5b3ae10b6e098a41b7fbd46f7570fc15
Gabriel <gabrielbrsc15@gmail.com>
2022-02-26 21:21:54 -0500

    BinarySearchCorrect

--- Algorithms/binarySearch.py
+++ Algorithms/binarySearch.py
@@ -1,30 +1,22 @@
 #https://youtu.be/DnvWAd-RGhk
-
-
 #return the last one that repeats
 
-list = [0, 1, 4, 4, 4, 5]
-target = 4
+list = [0, 1, 4, 4, 4, 5, 6, 9, 10]
+target = 9
 def binarySearch(list,target):
    firstIndex = 0
-   lastIndex = len(list) -1
+   lastIndex = len(list) - 1
    while lastIndex >= firstIndex:
        mediumIndex = firstIndex + (lastIndex - firstIndex)//2
        mediumValue = list[mediumIndex]
        if mediumValue == target:
-           if list[mediumIndex] == list[mediumIndex + 1]:
-               counter = 0
-               while list[mediumIndex] == list[mediumIndex + 1]:
-                   counter = counter + 1
-                   if list[mediumIndex] != list[mediumIndex + counter + 1]:
-                       return mediumIndex + counter
-
+           return mediumIndex
        elif target < mediumIndex:
            lastIndex = mediumIndex - 1
 
        else:
            firstIndex = mediumIndex + 1
-   return
+   return None
 
 print(binarySearch(list,target))
 
